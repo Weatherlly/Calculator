ebc8b37488031f07cdb3b92b4efe05dbc190c3e6
D4RKZ3R0 <81332273+Weatherlly@users.noreply.github.com>
2022-01-23 14:56:09 -0300

    Create app.js

---
+++ src/component/app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Display from './Display';
+import ButtonPanel from './ButtonPanel';
+import calculate from '../logic/calculate';
+import './App.css';
+import React from "react";
+import Display from "./Display";
+import ButtonPanel from "./ButtonPanel";
+import calculate from "../logic/calculate";
+import "./App.css";
+
+class App extends React.Component {
+  constructor(props) {
+@@ -14,19 +14,15 @@ class App extends React.Component {
+    };
+  }
