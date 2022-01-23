2457123388256c744add87dc45d1a427d2374636
D4RKZ3R0 <81332273+Weatherlly@users.noreply.github.com>
2022-01-23 14:57:20 -0300

    Update app.js

--- src/component/app.js
+++ src/component/app.js
@@ -14,3 +14,26 @@ class App extends React.Component {
 @@ -14,19 +14,15 @@ class App extends React.Component {
     };
   }
+
+  handleClick = (buttonName) => {
+  handleClick = buttonName => {
+    this.setState(calculate(this.state, buttonName));
+  }
+  };
+
+  render() {
+    return (
+      <div className="component-app">
+        <Display
+          value={this.state.next || this.state.total || '0'}
+        />
+        <ButtonPanel
+          clickHandler={this.handleClick}
+        />
+        <Display value={this.state.next || this.state.total || "0"} />
+        <ButtonPanel clickHandler={this.handleClick} />
+      </div>
+    );
+  }
+}
+export default App;
