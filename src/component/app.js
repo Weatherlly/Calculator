0980d1f4061ff68a3ff240130824716071914fea
D4RKZ3R0 <81332273+Weatherlly@users.noreply.github.com>
2022-01-23 15:05:10 -0300

    Update app.js

--- src/component/app.js
+++ src/component/app.js
@@ -1,8 +1,3 @@
-import React from 'react';
-import Display from './Display';
-import ButtonPanel from './ButtonPanel';
-import calculate from '../logic/calculate';
-import './App.css';
 import React from "react";
 import Display from "./Display";
 import ButtonPanel from "./ButtonPanel";
@@ -11,29 +6,24 @@ import "./App.css";
 
 class App extends React.Component {
   constructor(props) {
-@@ -14,19 +14,15 @@ class App extends React.Component {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
     };
   }
-
-  handleClick = (buttonName) => {
-  handleClick = buttonName => {
+handleClick = buttonName => {
     this.setState(calculate(this.state, buttonName));
-  }
   };
 
   render() {
     return (
       <div className="component-app">
-        <Display
-          value={this.state.next || this.state.total || '0'}
-        />
-        <ButtonPanel
-          clickHandler={this.handleClick}
-        />
-        <Display value={this.state.next || this.state.total || "0"} />
+              <Display value={this.state.next || this.state.total || "0"} />
         <ButtonPanel clickHandler={this.handleClick} />
       </div>
     );
   }
-}
+  }
 export default App;
